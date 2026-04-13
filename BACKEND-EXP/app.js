aa9a2ad51a55a2ac15b89caee900108327a59290
Dhruvishpatel188 <dhruvishpatel188@gmail.com>
2026-04-13 21:19:16 +0530

    commited

--- BACKEND-EXP/app.js
+++ BACKEND-EXP/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+const cors = require("cors")
+app.use(cors())
 
 const userRoutes = require("./src/routes/userRoutes");
 app.use("/user", userRoutes);
